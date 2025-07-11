d8fb268654fa95768b48d751c7b6642537777c34
minsu.lee <minsu.rob@gmail.com>
2025-07-11 21:38:05 +0900

    add: nest 기본버전으로 복귀

--- apps/be/src/app.module.ts
+++ apps/be/src/app.module.ts
@@ -1,36 +1,9 @@
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { GraphQLModule } from '@nestjs/graphql';
 import { Module } from '@nestjs/common';
-import { join } from 'path';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PostsModule } from './posts/posts.module';
-import { config } from 'dotenv';
-
-config();
 
 @Module({
-  imports: [
-    GraphQLModule.forRoot<ApolloDriverConfig>({
-      driver: ApolloDriver,
-      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-      sortSchema: true,
-      playground: true,
-    }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST || 'localhost',
-      port: parseInt(process.env.DB_PORT || '5432', 10),
-      username: process.env.DB_USERNAME || 'postgres',
-      password: process.env.DB_PASSWORD || 'password',
-      database: process.env.DB_NAME || 'sportcomm',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: false,
-      logging: true,
-    }),
-    PostsModule,
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })

d366b98943a70c43e6f560a828d33ca0dcc1a46e
minsu.lee <minsu.rob@gmail.com>
2025-09-24 10:47:04 +0900

    fix: expo eas 빌드세팅

--- apps/fe/app.config.js
+++ apps/fe/app.config.js
@@ -1,39 +1,38 @@
 module.exports = {
   expo: {
-    name: "sportcomm-fe-rnr",
-    slug: "sportcomm-fe-rnr",
+    name: "sportalkdev",
+    slug: "sportalkdev",
     version: "1.0.0",
     orientation: "portrait",
     icon: "./assets/images/icon.png",
-    scheme: "myapp",
-    userInterfaceStyle: "automatic",
-    newArchEnabled: true,
+    userInterfaceStyle: "light",
     splash: {
       image: "./assets/images/splash.png",
       resizeMode: "contain",
       backgroundColor: "#ffffff",
     },
     assetBundlePatterns: ["**/*"],
-    ios: {
-      supportsTablet: true,
-      bundleIdentifier: "com.releaseteamginger2.sportcommfernr",
-    },
-    android: {
-      adaptiveIcon: {
-        foregroundImage: "./assets/images/adaptive-icon.png",
-        backgroundColor: "#ffffff",
-      },
-      package: "com.releaseteamginger2.sportcommfernr",
-    },
     web: {
       bundler: "metro",
       output: "static",
       favicon: "./assets/images/favicon.png",
     },
+    ios: {
+      bundleIdentifier: "com.releaseteamginger2.sportalkdev", // iOS용 고유 식별자
+      supportsTablet: true // 필요에 따라 true/false
+    },
+    android: {
+      package: "com.releaseteamginger2.sportalkdev"
+    },
     plugins: ["expo-router"],
     experiments: {
       typedRoutes: true,
     },
-    extra: {},
+    extra: {
+      "eas": {
+        "projectId": "289a6b7c-67b0-4b74-adf0-d9ea50fdd875"
+      }
+    },
+    owner: "releaseteamginger2",
   },
 };
